6653852db6e636580986a47588612827a6488a05
Federico Pascual Fioriti <ffioriti10@gmail.com>
2022-12-02 19:39:16 -0300

    main

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,30 @@
 #include "iri.cpp"
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <funciones.h>
 
-int main() {
+using namespace std;
 
-    std::cout << "Hello World TP_Final";
-
+int main(int argc, char** argv)
+{
+    int tamP = 0;
+    string nombrearchivados;
+    Paciente* pacientes = new Paciente[tamP];
+    leer(pacientes, tamP);
+    int tamv = 0;
+    int tama = 0;
+    Paciente* vigentes = new Paciente[tamv];
+    Paciente* archivados = new Paciente[tama];
+    cout << "Ingrese nombre del archivo archivados con su extension :" << endl;
+    cin >> nombrearchivados;
+    vigentyarch(pacientes, tamP, archivados, vigentes, tama, tamv);
+    secretaria(vigentes, tamv, archivados, tama);
+    archivoarchi(archivados, tama, nombrearchivados);
+    delete[]pacientes;
+    delete[]vigentes;
+    delete[]archivados;
 
     return EXIT_SUCCESS;
 }
+
