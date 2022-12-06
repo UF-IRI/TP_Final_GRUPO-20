9158857526bc39f6a47c350b53174129cec5cf48
Federico Pascual Fioriti <ffioriti10@gmail.com>
2022-12-06 19:31:29 -0300

    unit terminado

--- unit-test/main_test.cpp
+++ unit-test/main_test.cpp
@@ -10,44 +10,43 @@ namespace CasosBase {
 	TEST(CasosBase, AgregarPaciente)
 	{
 		int N = 2;
-		Paciente* ejemplo = new Pacinte[N];
+		Paciente* ejemplo = new Paciente[N];
 
-		ejemplo[0].dni = "45238330";
+		ejemplo[0].dni = 45238330;
 		ejemplo[0].nombre = "Juan";
-		ejemplo[0].apellido = “Guaglianone”;
-		ejemplo[0].obra_social = “osde”;
-		ejemplo[0].genero = “m”;
-		ejemplo[0].con.dni = “45238330”;
-		ejemplo[0].con.mail = “juanpiguaglianone@gmail.com”;
-		ejemplo[0].con.celular = “ + 54(147)34982983”;
-		ejemplo[0].con.telefono = “ + 54(974)82014567”;
-		ejemplo[0].con.direccion = “Bouchard 1257”;
-		ejemplo[0].consulta.dni = “45238330”;
-		ejemplo[0].consulta.fecha_solicitado = “5 / 12 / 2022”;
-		ejemplo[0].consulta.fechaturno = “7 / 12 / 2022”;
-		ejemplo[0].consulta.presencialidad = “0”;
-		ejemplo[0].consulta.matricula_med = “11 - 089 - 2367”;
-		ejemplo[0].nac = “Argentina”;
-		ejemplo[0].estado = “internado”;
+		ejemplo[0].apellido = "Guaglianone";
+		ejemplo[0].obra_social = "osde";
+		ejemplo[0].genero = 'm';
+		ejemplo[0].con.dni = 45238330;
+		ejemplo[0].con.mail = "juanpiguaglianone@gmail.com";
+		ejemplo[0].con.celular = " + 54(147)34982983";
+		ejemplo[0].con.telefono = "+ 54(974)82014567";
+		ejemplo[0].con.direccion = "Bouchard";
+		ejemplo[0].consulta.dni = 45238330;
+		ejemplo[0].consulta.fecha_solicitado = "5/12/2022";
+		ejemplo[0].consulta.fechaturno = 1245365257;
+		ejemplo[0].consulta.presencialidad = 0;
+		ejemplo[0].consulta.matricula_med = "11-089-2367";
+		ejemplo[0].nac = "12/2/1970";
+		ejemplo[0].estado = "internado";
 
-		ejemplo[1].dni = “43892783”;
-		ejemplo[1].nombre = “Maria”;
-		ejemplo[1].apellido = “Namir”;
-		ejemplo[1].tipoDeDoc = “dni”;
-		ejemplo[1].obra_social = “swiss medical”;
-		ejemplo[1].genero = “f”;
-		ejemplo[1].con.dni = “43892783”;
-		ejemplo[1].con.mail = “Mnamir@gmail.com”;
-		ejemplo[1].con.celular = “ + 54(176)68420326”;
-		ejemplo[1].con.telefono = “ + 54(801)04193744”;
-		ejemplo[1].con.direccion = “Rosales 895”;
-		ejemplo[1].consulta.dni = “43892783”;
-		ejemplo[1].consulta.fecha_solicitado = “5 / 12 / 2022”;
-		ejemplo[1].consulta.fechaturno = “23 / 01 / 2023”;
-		ejemplo[1].consulta.presencialidad = “1”;
-		ejemplo[1].consulta.matricula_med = “87 - 079 - 3429”;
-		ejemplo[1].nac = “India”;
-		ejemplo[1].estado = “fallecido”;
+		ejemplo[1].dni = 43892783;
+		ejemplo[1].nombre = "Maria";
+		ejemplo[1].apellido = "Namir";
+		ejemplo[1].obra_social = "swiss medical";
+		ejemplo[1].genero = 'f';
+		ejemplo[1].con.dni = 43892783;
+		ejemplo[1].con.mail = "Mnamir@gmail.com";
+		ejemplo[1].con.celular = "+ 54(176)68420326";
+		ejemplo[1].con.telefono = "+ 54(801)04193744";
+		ejemplo[1].con.direccion = "Rosales";
+		ejemplo[1].consulta.dni = 43892783;
+		ejemplo[1].consulta.fecha_solicitado = "5/12/2022";
+		ejemplo[1].consulta.fechaturno = 1565365817;
+		ejemplo[1].consulta.presencialidad = 1;
+		ejemplo[1].consulta.matricula_med = "87-079-3429";
+		ejemplo[1].nac = "7/2/2000";
+		ejemplo[1].estado = "fallecido";
 
 
 
@@ -55,33 +54,31 @@ namespace CasosBase {
 
 		Paciente aux;
 
-		aux.dni = “34982378”;
-		aux.nombre = “Jesus”;
-		aux.apellido = “Messi”;
-		aux.tipoDeDoc = “dni”;
-		aux.obra_social = “pasteur”;
-		aux.genero = “m”;
-		aux.con.dni = “34982378”;
-		aux.con.mail = “MessiJ@gmail.com”;
-		aux.con.celular = “ + 54(345)56489374)”;
-		aux.con.telefono = “ + 54(262)26039091”;
-		aux.con.direccion = “Mitre 734”
-			aux.consulta.dni = “34982378”;
-		aux.consulta.fecha_solicitado = “05 / 12 / 2022”;
-		aux.consulta.fechaturno = “14 / 02 / 2023”;
-		aux.consulta.presencialidad = “1”;
-		aux.consulta.matricula_med = “90 - 092 - 5729”;
-		aux.nac = “Uruguaya”;
-		aux.estado = “vigente”;
+		aux.dni = 34982378;
+		aux.nombre = "Jesus";
+		aux.apellido = "Messi";
+		aux.obra_social = "pasteur";
+		aux.genero = 'm';
+		aux.con.dni = 34982378;
+		aux.con.mail = "MessiJ@gmail.com";
+		aux.con.celular = " + 54(345)56489374)";
+		aux.con.telefono = " + 54(262)26039091";
+		aux.con.direccion = "Mitre";
+		aux.consulta.dni = 34982378;
+		aux.consulta.fecha_solicitado = "05/12/2022";
+		aux.consulta.fechaturno = 1490365817;
+		aux.consulta.presencialidad = 1;
+		aux.consulta.matricula_med = "90-092-5729";
+		aux.nac = "25/10/2003";
+		aux.estado = "n/c";
 
 
 		agregarPaciente(ejemplo, aux, &N);
 
-		EXPECT_EQ(*N, 3);
+		EXPECT_EQ(N, 3);
 		EXPECT_EQ(ejemplo[2].dni, aux.dni);
 		EXPECT_EQ(ejemplo[2].nombre, aux.nombre);
 		EXPECT_EQ(ejemplo[2].apellido, aux.apellido);
-		EXPECT_EQ(ejemplo[2].tipoDeDoc, aux.tipoDeDoc);
 		EXPECT_EQ(ejemplo[2].obra_social, aux.obra_social);
 		EXPECT_EQ(ejemplo[2].genero, aux.genero);
 		EXPECT_EQ(ejemplo[2].con.dni, aux.con.dni);
@@ -106,16 +103,16 @@ namespace CasosBase {
 	{
 		int n = 1;
 		UltimaConsulta* ejemplo = new UltimaConsulta[n];
-
-		Consultas(ejemplo, n);
-
 		UltimaConsulta aux;
 
-		aux.dni = “43963883”;
-		aux.fecha_solicitado = “14 / 05 / 1987”;
-		aux.fechaturno = “30 / 7 / 1987”;
-		aux.presencialidad = “0”;
-		aux.matricula_med = “11 - 089 - 2367”;
+		aux.dni = 43963883;
+		aux.fecha_solicitado = "14/05/1987";
+		aux.fechaturno = 1470363817;
+		aux.presencialidad = 0;
+		aux.matricula_med = "11-089-2367";
+
+		ejemplo[0] = aux;
+		Consultas(ejemplo, n);
 
 		EXPECT_EQ(ejemplo[0].dni, aux.dni);
 		EXPECT_EQ(ejemplo[0].fecha_solicitado, aux.fecha_solicitado);
@@ -130,78 +127,74 @@ namespace CasosBase {
 	TEST(CasosBase, EliminarVigente)
 	{
 		int N = 3;
-		Paciente* array = new Pacinte[N];
+		Paciente* array = new Paciente[N];
 
-		array[0].dni = “45238330”;
-		array[0].nombre = “Juan”;
-		array[0].apellido = “Guaglianone”;
-		array[0].tipoDeDoc = “dni”;
-		array[0].obra_social = “osde”;
-		array[0].genero = “m”;
-		array[0].con.dni = “45238330”;
-		array[0].con.mail = “juanpiguaglianone@gmail.com”;
-		array[0].con.celular = “ + 54(147)34982983”;
-		array[0].con.telefono = “ + 54(974)82014567”;
-		array[0].con.direccion = “Bouchard 1257”;
-		array[0].consulta.dni = “45238330”;
-		array[0].consulta.fecha_solicitado = “5 / 12 / 2022”;
-		array[0].consulta.fechaturno = “7 / 12 / 2022”;
-		array[0].consulta.presencialidad = “0”;
-		array[0].consulta.matricula_med = “11 - 089 - 2367”;
-		array[0].nac = “Argentina”;
-		array[0].estado = “internado”;
+		array[0].dni = 45238330;
+		array[0].nombre = "Juan";
+		array[0].apellido = "Guaglianone";
+		array[0].obra_social = "osde";
+		array[0].genero = 'm';
+		array[0].con.dni = 45238330;
+		array[0].con.mail = "juanpiguaglianone@gmail.com";
+		array[0].con.celular = "+ 54(147)34982983";
+		array[0].con.telefono = "+ 54(974)82014567";
+		array[0].con.direccion = "Bouchard";
+		array[0].consulta.dni = 45238330;
+		array[0].consulta.fecha_solicitado = "5/12/2022";
+		array[0].consulta.fechaturno = 1356865257;
+		array[0].consulta.presencialidad = 0;
+		array[0].consulta.matricula_med = "11-089-2367";
+		array[0].nac = "23/8/2014";
+		array[0].estado = "internado";
 
-		array[1].dni = “43892783”;
-		array[1].nombre = “Maria”;
-		array[1].apellido = “Namir”;
-		array[1].tipoDeDoc = “dni”;
-		array[1].obra_social = “swiss medical”;
-		array[1].genero = “f”;
-		array[1].con.dni = “43892783”;
-		array[1].con.mail = “Mnamir@gmail.com”;
-		array[1].con.celular = “ + 54(176)68420326”;
-		array[1].con.telefono = “ + 54(801)04193744”;
-		array[1].con.direccion = “Rosales 895”;
-		array[1].consulta.dni = “43892783”;
-		array[1].consulta.fecha_solicitado = “5 / 12 / 2022”;
-		array[1].consulta.fechaturno = “23 / 01 / 2023”;
-		array[1].consulta.presencialidad = “1”;
-		array[1].consulta.matricula_med = “87 - 079 - 3429”;
-		array[1].nac = “India”;
-		array[1].estado = “fallecido”;
+		array[1].dni = 43892783;
+		array[1].nombre = "Maria";
+		array[1].apellido = "Namir";
+		array[1].obra_social = "swiss medical";
+		array[1].genero = 'f';
+		array[1].con.dni = 43892783;
+		array[1].con.mail = "Mnamir@gmail.com";
+		array[1].con.celular = "+ 54(176)68420326";
+		array[1].con.telefono = "+ 54(801)04193744";
+		array[1].con.direccion = "Rosales";
+		array[1].consulta.dni = 43892783;
+		array[1].consulta.fecha_solicitado = "5/12/2022";
+		array[1].consulta.fechaturno = 1457865137;
+		array[1].consulta.presencialidad = 1;
+		array[1].consulta.matricula_med = "87-079-3429";
+		array[1].nac = "11/9/2001";
+		array[1].estado = "fallecido";
 
-		array[2].dni = “34982378”;
-		array[2].nombre = “Jesus”;
-		array[2].apellido = “Messi”;
-		array[2].tipoDeDoc = “dni”;
-		array[2].obra_social = “pasteur”;
-		array[2].genero = “m”;
-		array[2].con.dni = “34982378”;
-		array[2].con.mail = “MessiJ@gmail.com”;
-		array[2].con.celular = “ + 54(345)56489374)”;
-		array[2].con.telefono = “ + 54(262)26039091”;
-		array[2].con.direccion = “Mitre 734”;
-		array[2].consulta.dni = “34982378”;
-		array[2].consulta.fecha_solicitado = “05 / 12 / 2022”;
-		array[2].consulta.fechaturno = “14 / 02 / 2023”;
-		array[2].consulta.presencialidad = “1”;
-		array[2].consulta.matricula_med = “90 - 092 - 5729”;
-		array[2].nac = “Uruguaya”;
-		array[2].estado = “vigente”;
+		array[2].dni = 34982378;
+		array[2].nombre = "Jesus";
+		array[2].apellido = "Messi";
+		array[2].obra_social = "pasteur";
+		array[2].genero = 'm';
+		array[2].con.dni = 34982378;
+		array[2].con.mail = "MessiJ@gmail.com";
+		array[2].con.celular = "+ 54(345)56489374)";
+		array[2].con.telefono = "+ 54(262)26039091";
+		array[2].con.direccion = "Mitre";
+		array[2].consulta.dni = 34982378;
+		array[2].consulta.fecha_solicitado = "05/12/2022";
+		array[2].consulta.fechaturno = 1670364535;
+		array[2].consulta.presencialidad = 1;
+		array[2].consulta.matricula_med = "90-092-5729";
+		array[2].nac = "19/5/2018";
+		array[2].estado = "n/c";
 
-		cantEliminar = 2;
+		int cantEliminar = 2;
 
 		eliminarVigente(array, &N, cantEliminar);
 
 
 
-		EXPECT_EQ(&N, 1);
+		EXPECT_EQ(N, 1);
 
-		For(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			EXPECT_EQ(array[i].dni, array[0].dni);
 			EXPECT_EQ(array[i].nombre, array[0].nombre);
 			EXPECT_EQ(array[i].apellido, array[0].apellido);
-			EXPECT_EQ(array[i].tipoDeDoc, array[0].tipodeDoc);
 			EXPECT_EQ(array[i].obra_social, array[0].obra_social);
 			EXPECT_EQ(array[i].genero, array[0].genero);
 			EXPECT_EQ(array[i].con.dni, array[0].con.dni);
@@ -217,3 +210,5 @@ namespace CasosBase {
 			EXPECT_EQ(array[i].nac, array[0].nac);
 			EXPECT_EQ(array[i].estado, array[0].estado);
 		}
+	}
+}
